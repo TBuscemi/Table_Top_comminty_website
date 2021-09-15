8a464113f7ab5ed75edef08e49e24002db7205a6
TBuscemi <thomasbuscemi124@gmail.com>
2021-09-15 14:20:27 -0400

    Went in fixed a few spelling mistakes that were causing it to not run right.

--- table_top/authentication/serializers.py
+++ table_top/authentication/serializers.py
@@ -1,4 +1,3 @@
-from typing_extensions import Required
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
@@ -6,7 +5,7 @@ from django.contrib.auth.password_validation import validate_password
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryest=User.objects.all())])
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
 password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
 
 class Meta:
