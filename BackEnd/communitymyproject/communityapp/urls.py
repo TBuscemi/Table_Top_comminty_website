f091bd869198bff54ff02577e8ba9c437dc72639
TBuscemi <thomasbuscemi124@gmail.com>
2021-09-15 12:35:20 -0400

    back end re-work due to technical issues.

--- BackEnd/communitymyproject/communityapp/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views 
-from django.urls import path, include
-
-app_name ='communityapp'
-urlpatterns = [
-    path('',views.index,name=views.index)
-]
--- table_top/table_top/wsgi.py
+++ table_top/table_top/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for communitymyproject project.
+WSGI config for table_top project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'communitymyproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'table_top.settings')
 
 application = get_wsgi_application()
