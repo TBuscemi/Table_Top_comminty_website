63e3b16e877f8e07b020e07eefdb1f2a20b7742e
TBuscemi <thomasbuscemi124@gmail.com>
2021-09-17 16:03:49 -0400

    added the imports that i need in views for friends_list/user_chat/user_friends

--- table_top/user_chat/views.py
+++ table_top/user_chat/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.db.models.query import QuerySet
+from django.http import response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from .models import Chat
+from .serializers import ChatSerializer
+from django.contrib.auth.models import User
